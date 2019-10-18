6a17574d5ff45ba45a4cbbf797bf095a89ea55ca
Blake Tompkins <blaketompkins99@gmail.com>
2019-10-18 14:50:17 -0600

    Finish initial CSS formatting

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+// Setting Variables
+var players = ["BIRD", "CARTER", "ANTETOKOUNMPO", "HARDEN", "JORDAN", "LEBRON", "WESTBROOK", "YOUNG"];
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+"u", "v", "w", "x", "y", "z"];
+
+var word = Math.floor(Math.random() * 7) + 1;
+
+console.log(blake);
+
+
