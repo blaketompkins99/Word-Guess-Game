1055a1d60c05ec562d4515929b0889afd02f4a60
Blake Tompkins <blaketompkins99@gmail.com>
2019-10-19 19:01:13 -0600

    Updated JavaScript to set up variables and display underscores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,59 @@
 // Setting Variables
 var players = ["BIRD", "CARTER", "ANTETOKOUNMPO", "HARDEN", "JORDAN", "LEBRON", "WESTBROOK", "YOUNG"];
 
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-"u", "v", "w", "x", "y", "z"];
+var guessLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+"U", "V", "W", "X", "Y", "Z"];
 
-var word = Math.floor(Math.random() * 7) + 1;
+var wins = 0;
+var remainingGuesses = 12;
 
-console.log(blake);
+// Function for generating the word
+function newWord (){
+
+    // Picks random player from players list
+    var word = players[Math.floor(Math.random(players) * 8) + 1];
+
+    // Loop to set the number of underscores for the word
+    for(i=0; i < word.length; i++) {
+        var underscore = document.getElementById("hiddenWord");
+        underscore.textContent += "_ ";
+    }
+}
+
+// Function for updating the score
+function scoreUpdate() {
+    wins += 1;
+    document.getElementById("winCounter").textContent = wins;
+}
+
+// Function updating remaining guesses
+function guessesLeft() {
+    document.getElementById("guessCounter").append = remainingGuesses;
+}
+
+newWord();
+
+// document.onkeyup()
+
+for (i=0; i > remainingGuesses; remainingGuesses--) {
+    
+    document.onkeyup = function(event) {
+        // Variable for the guesses
+        var userGuess = event.key.toUpperCase();
+        if(userGuess === word) {
+            remainingGuesses += 1;
+            guessesLeft();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+// console.log(word);
 
 
