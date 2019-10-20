7e0d4ca83a934ded42819054e135b7a2942ea427
Blake Tompkins <blaketompkins99@gmail.com>
2019-10-19 21:53:13 -0600

    Attempted to finish the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,29 @@
 
 // Setting Variables
 var players = ["BIRD", "CARTER", "ANTETOKOUNMPO", "HARDEN", "JORDAN", "LEBRON", "WESTBROOK", "YOUNG"];
-
-var guessLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
 "U", "V", "W", "X", "Y", "Z"];
-
+var guessedLetters = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_",
+"_", "_", "_", "_", "_", "_"]
+var wrongGuesses = "";
 var wins = 0;
 var remainingGuesses = 12;
+var underscore = "";
 
 // Function for generating the word
-function newWord (){
+function newGame (){
+
+    // Resets Variables
+    underscore = "";
+    remainingGuesses = 12;
 
     // Picks random player from players list
-    var word = players[Math.floor(Math.random(players) * 8) + 1];
+    var word = players[Math.floor(Math.random(players) * 8)];
 
     // Loop to set the number of underscores for the word
     for(i=0; i < word.length; i++) {
-        var underscore = document.getElementById("hiddenWord");
-        underscore.textContent += "_ ";
+        underscore = underscore + "_ ";
+        document.getElementById("hiddenWord").textContent = underscore;
     }
 }
 
@@ -29,32 +35,35 @@ function scoreUpdate() {
 
 // Function updating remaining guesses
 function guessesLeft() {
-    document.getElementById("guessCounter").append = remainingGuesses;
+    remainingGuesses--;
+    document.getElementById("guessCounter").textContent = remainingGuesses;
 }
 
-newWord();
+newGame();
 
-// document.onkeyup()
-
-for (i=0; i > remainingGuesses; remainingGuesses--) {
-    
+if(underscore === word) {
+    scoreUpdate();
+    newGame();
+} else if (remainingGuesses === 0){
+    newGame();
+} else {
+  
     document.onkeyup = function(event) {
         // Variable for the guesses
         var userGuess = event.key.toUpperCase();
-        if(userGuess === word) {
-            remainingGuesses += 1;
-            guessesLeft();
-        }
 
+        for (i=0; i < word.length(); i++) {
+            if(userGuess === word.charAt(i) && underscore.charAt(i) === "_") {
+                underscore.charAt(i) = userGuess;
+                document.getElementById("hiddenWord").textContent = underscore;
+            } else {
+                if(userGuess === letters[i] && guessedLetters[i] === "_"){
+                    guessedLetters[i] = userGuess;
+                    wrongGuesses += guessedLetters + " ";
+                    document.getElementById("guessedLetters").textContent = guessedLetters;
+                    guessesLeft();
+                }
+            }
+        }
     }
 }
-
-
-
-
-
-
-
-// console.log(word);
-
-
